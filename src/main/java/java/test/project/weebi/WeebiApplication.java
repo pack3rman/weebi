10f794b8cea00c27269147362c20c48746630d8c
pack3 <pack3rman@gmail.com>
2023-09-21 20:39:03 +0200

    [W-1] Start Project

---
+++ src/main/java/java/test/project/weebi/WeebiApplication.java
@@ -0,0 +1,13 @@
+package java.test.project.weebi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeebiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeebiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/test/project/weebi/WeebiApplicationTests.java
@@ -0,0 +1,13 @@
+package java.test.project.weebi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeebiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
